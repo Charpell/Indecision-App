5322485da840388da8b1b8a911e1d18784c9cfa1
Charpell <charpellumeh@gmail.com>
2018-04-18 20:52:11 +0100

    Connfigure babel to support Class properties

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,16 +6,8 @@ import Action from './Action';
 import Options from './Options';
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-  
-    this.state = {
-      options: []
-    }
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.handleDeleteOption = this.handleDeleteOption.bind(this)
+  state = {
+    options: []
   }
 
   componentDidMount() {
@@ -39,23 +31,23 @@ export default class IndecisionApp extends React.Component {
     }
   }
 
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }))
   }
 
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option)
     }))
   }
 
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum]
     alert(option)
   }
 
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if(!option) {
       return 'Enter valid value to add item';
     } else if (this.state.options.indexOf(option) > -1) {
